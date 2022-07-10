fb285d84e792ea0475727cfdb41567b3a81458f5
Lukasz Szczepka <l.szczepka@rescop.com>
2022-07-10 23:09:34 +0200

    Adding GoogleMainPage and GoogleSidebar classes.

---
+++ SeleniumAndSpecflowPageModel/Google/GoogleSidebar.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using SeleniumAndSpecflowPageModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumAndSpecflowPageModel.Google
+{
+    public class GoogleSidebar : PageObject
+    {
+        public GoogleSidebar(IWebDriver webDriver) : base(webDriver)
+        {
+        }
+
+        private const string sidebarXPath = "//div[@class='gb_Vd gb_Xa gb_Kd']";
+
+        private Element sidebar => driver.FindElement(By.XPath(sidebarXPath));
+
+        public override bool IsLoaded()
+        {
+            return IsElementDisplayed(sidebar);
+        }
+    }
+}
