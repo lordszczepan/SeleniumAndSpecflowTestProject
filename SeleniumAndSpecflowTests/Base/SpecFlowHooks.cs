e1e2984f5a6be2b01ce5a2a9229f193028caa5d8
Lukasz Szczepka <l.szczepka@rescop.com>
2022-07-24 16:20:34 +0200

    test

--- SeleniumAndSpecflowTests/Base/SpecFlowHooks.cs
+++ SeleniumAndSpecflowTests/Base/SpecFlowHooks.cs
@@ -12,11 +12,24 @@ namespace SeleniumAndSpecflowTests.Base
     [Binding]
     public class SpecFlowHooks : TestsBase
     {
+        private static string tekst;
 
         public SpecFlowHooks(TestsSettings settings) : base(settings)
         {
         }
 
+        [BeforeTestRun]
+        public static void InitializeReport()
+        {
+            tekst = "TEST";
+            Console.WriteLine(tekst);
+        }
 
+        [BeforeFeature]
+        public static void BeforeReport()
+        {
+            tekst = "TEST 2";
+            Console.WriteLine(tekst);
+        }
     }
 }
