779d2726f2e9a3329287c9afc5e8b0ddb4b8f4f4
Lukasz Szczepka <l.szczepka@rescop.com>
2022-07-24 12:37:47 +0200

    Improvements in GoogleMainImagesPage class. Adding GoogleSearchImageResultsPage class. Adding GoToImages() method in GoogleSidebar. Adding 'Should search for image on Google page' test case with all necessary methods.

---
+++ SeleniumAndSpecflowPageModel/Google/GoogleSearchImageResultsPage.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using SeleniumAndSpecflowPageModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumAndSpecflowPageModel.Google
+{
+    public class GoogleSearchImageResultsPage : PageObject
+    {
+        public GoogleSearchImageResultsPage(IWebDriver webDriver) : base(webDriver)
+        {
+        }
+
+        private const string pnlSearchResultsID = "gsr";
+
+        private Element pnlSearchResults => driver.FindElement(By.Id(pnlSearchResultsID));
+
+        private Element txtSearch => driver.FindElement(By.XPath("//*[@id='sf']/div[1]//input"));
+
+        public override bool IsLoaded()
+        {
+            return IsElementDisplayed(pnlSearchResults);
+        }
+
+        public string ReturnSearchTextBoxValue()
+        {
+            return txtSearch.GetAttribute("value");
+        }
+    }
+}
